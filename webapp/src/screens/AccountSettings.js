6612bb23c0ed9ecd6bec1bbe3034dbac9acc025c
Masudul Haque <saadixl@outlook.com>
2023-11-14 23:25:38 +0800

    Add google login

--- webapp/src/screens/AccountSettings.js
+++ webapp/src/screens/AccountSettings.js
@@ -1,25 +1,52 @@
+import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import GoogleIcon from "@mui/icons-material/Google";
 import LogoutIcon from "@mui/icons-material/Logout";
 import BasicLayout from "../layouts/BasicLayout";
+import { signInWithGoogle, signOut } from "../services/auth";
+import { auth } from "../services/firebase";
 
 function AccountSettings() {
+  const [name, setName] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setName(user.displayName);
+      } else {
+        setName();
+      }
+    });
+  }, []);
+
+  let settingComponents = name ? (
+    <>
+      <p>Logged in as {name}</p>
+      <Button
+        onClick={signOut}
+        startIcon={<LogoutIcon />}
+        size="large"
+        variant="contained"
+        color="error"
+      >
+        Logout
+      </Button>
+    </>
+  ) : (
+    <Button
+      onClick={signInWithGoogle}
+      startIcon={<GoogleIcon />}
+      size="large"
+      variant="contained"
+    >
+      Login with Google
+    </Button>
+  );
   return (
     <BasicLayout screenName="Account settings" activeScreen="accountsettings">
       <Grid container spacing={3}>
         <Grid item xs={12} md={8} lg={9}>
-          <Button startIcon={<GoogleIcon />} size="large" variant="contained">
-            Login with Google
-          </Button>
-          <Button
-            startIcon={<LogoutIcon />}
-            size="large"
-            variant="contained"
-            color="error"
-          >
-            Logout
-          </Button>
+          {settingComponents}
         </Grid>
       </Grid>
     </BasicLayout>
--- webapp/src/services/firebase.js
+++ webapp/src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDT9jz5cqMR_4_4vOJuu2Zroqv_zsRHQkk",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+const auth = getAuth();
 
-export { db };
+export { auth, db };
